b81bb14e229d0504eb0dc28a4446417ea7e19739
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-04-02 12:38:07 -0400

    finished mongoose implementation

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+
+    day: {type:Date, 
+        default:new Date()},
+
+    exercises: [{
+
     type: {
       type: String,
       trim: true,
@@ -29,4 +35,10 @@ const workoutSchema = new Schema({
       type: Number,
       required: "Enter number of sets"
     }
+}]
+
   });
+
+  const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
