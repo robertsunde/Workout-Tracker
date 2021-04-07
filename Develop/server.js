49f94ad5e536f4e3ee2da4cb9b96286bec94675d
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-04-06 21:10:53 -0400

    having issues with heroku, commented out db connect for heroku until solved

--- Develop/server.js
+++ Develop/server.js
@@ -11,24 +11,24 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
 
 
 
 
-// mongoose.connect("mongodb://localhost:27017/workout", {
-// // useUnifiedTopology: true,
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect("mongodb://localhost:27017/workout", {
+// useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/apiroutes"));
